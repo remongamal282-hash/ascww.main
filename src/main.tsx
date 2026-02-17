c47d33a9d3e98a2ab46294994964ceb3e6e2e6f5
Remon <remongamal282@gamil.com>
2026-02-17 12:09:24 +0200

    update email & phone & responsive css last

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './input.css';
+import '../css/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
